a90fa2c9004e42dd8f7e954b7a59c46f1b0b3460
andylushman <andylushman@gmail.com>
2018-10-26 12:53:08 -0600

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import Fighter from "./components/figher";
+import Match from "./components/match";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <Fighter />;
+    return (
+      <div>
+        <Fighter />
+        <Match />
+      </div>
+    );
   }
 }
 
---
+++ src/components/match.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Match extends Component {
+  render() {
+    return <div>Match Component</div>;
+  }
+}
+
+export default Match;
