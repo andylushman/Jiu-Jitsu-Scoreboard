1c11176be21bae91c86b3183affc62d480d06936
andylushman <andylushman@gmail.com>
2018-10-30 16:33:36 -0600

    added buttons for clock

--- src/components/timer.js
+++ src/components/timer.js
@@ -5,8 +5,8 @@ export class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      minutesRemaining: 0,
-      secondsRemaining: 0,
+      minutesRemaining: 1,
+      secondsRemaining: 13,
       totalTime: 0
     };
 
@@ -16,6 +16,8 @@ export class Timer extends Component {
     this.resetTimer = this.resetTimer.bind(this);
     this.increaseMin = this.increaseMin.bind(this);
     this.decreaseMin = this.decreaseMin.bind(this);
+    this.increaseSec = this.increaseSec.bind(this);
+    this.decreaseSec = this.decreaseSec.bind(this);
   }
 
   async increaseMin() {
@@ -34,6 +36,29 @@ export class Timer extends Component {
 
     await this.setState({
       minutesRemaining: this.state.minutesRemaining - 1
+    });
+
+    this.setState({
+      totalTime: this.state.minutesRemaining + this.state.secondsRemaining
+    });
+  }
+
+  async increaseSec() {
+    await this.setState({
+      secondsRemaining: this.state.secondsRemaining + 1
+    });
+    this.setState({
+      totalTime: this.state.minutesRemaining + this.state.secondsRemaining
+    });
+  }
+
+  async decreaseSec() {
+    if (this.state.secondsRemaining <= 0) {
+      return;
+    }
+
+    await this.setState({
+      secondsRemaining: this.state.secondsRemaining - 1
     });
 
     this.setState({
@@ -66,10 +91,25 @@ export class Timer extends Component {
 
   render() {
     return (
-      <div className="container">
-        <button onClick={this.increaseMin}>+</button>
-        <button onClick={this.decreaseMin}>-</button>
-        <div className="timer">{this.state.totalTime}</div>
+      <div className="container timer">
+        <div className="row">
+          <div className="col timer-min">
+            <div className="timer-min-btn">
+              <button onClick={this.increaseMin}>+</button>
+              <button onClick={this.decreaseMin}>-</button>
+              <div className="col">Min</div>
+            </div>
+            <div className="timer-clock">{this.state.minutesRemaining}:</div>
+          </div>
+          <div className="col timer-sec">
+            <div className="timer-sec-btn">
+              <button onClick={this.increaseSec}>+</button>
+              <button onClick={this.decreaseSec}>-</button>
+              <div className="col">Sec</div>
+            </div>
+            <div className="timer-clock">{this.state.secondsRemaining}</div>
+          </div>
+        </div>
         <button onClick={this.startTimer}>Start</button>
         <button onClick={this.stopTimer}>Stop</button>
         <button onClick={this.resetTimer}>Reset</button>
