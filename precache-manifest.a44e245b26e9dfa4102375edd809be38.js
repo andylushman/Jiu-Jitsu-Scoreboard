ba4f9523babefc1a75a0d1f92d429b6c26fb8d13
andylushman <andylushman@gmail.com>
2018-11-01 11:39:02 -0600

    Updates

---
+++ precache-manifest.a44e245b26e9dfa4102375edd809be38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ff1f710e2031c8877fa8bb316e4cf3d",
+    "url": "/Jiu-Jitsu-Scoreboard/static/media/gear-icon.4ff1f710.png"
+  },
+  {
+    "revision": "996d8ac479253a079fad",
+    "url": "/Jiu-Jitsu-Scoreboard/static/js/runtime~main.996d8ac4.js"
+  },
+  {
+    "revision": "0cdbc3f89f424e625687",
+    "url": "/Jiu-Jitsu-Scoreboard/static/js/main.0cdbc3f8.chunk.js"
+  },
+  {
+    "revision": "5144c14fa45eb0d062fb",
+    "url": "/Jiu-Jitsu-Scoreboard/static/js/1.5144c14f.chunk.js"
+  },
+  {
+    "revision": "0cdbc3f89f424e625687",
+    "url": "/Jiu-Jitsu-Scoreboard/static/css/main.ca0f378a.chunk.css"
+  },
+  {
+    "revision": "05ee26ab7f0895033d5f930c7dbf3e6c",
+    "url": "/Jiu-Jitsu-Scoreboard/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Jiu-Jitsu-Scoreboard/precache-manifest.a44e245b26e9dfa4102375edd809be38.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Jiu-Jitsu-Scoreboard/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
